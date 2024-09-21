0c77ad7bd5ae73ec3121f73d16d8be362051e7c7
Rolly_Program <robertrolison99@gmail.com>
2024-09-20 18:01:54 -0600

    database

--- golf-tee-times-backend/src/server.js
+++ golf-tee-times-backend/src/server.js
@@ -1,6 +1,10 @@
 // src/server.js
 const app = require('./app');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 3001;
+
+// Connect to the database
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
