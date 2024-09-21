57230caddc1aa02b3432ac6663c79c65cd543703
Rolly_Program <robertrolison99@gmail.com>
2024-09-20 18:03:36 -0600

    setup

--- golf-tee-times-backend/src/controllers/teeTimesController.js
+++ golf-tee-times-backend/src/controllers/teeTimesController.js
@@ -1,9 +1,9 @@
 // src/controllers/teeTimesController.js
-const { fetchTeeTimes } = require('../services/teeTimesService');
+const TeeTime = require('../models/TeeTime');
 
 const getTeeTimes = async (req, res) => {
   try {
-    const teeTimes = await fetchTeeTimes();
+    const teeTimes = await TeeTime.find();
     res.json({ teeTimes });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch tee times' });
